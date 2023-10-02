70527348b835c00db1b3ed6226ab047d7708ad3b
Chai Phonbopit <Phonbopit@gmail.com>
2023-10-02 20:21:17 +0700

    chore: setup rust server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use ethers::prelude::Provider;
+
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
+    dotenv().ok();
+    let rpc_url = std::env::var("RPC_URL").expect("RPC_URL must be set.");
+    let provider = Provider::try_from(rpc_url)?;
+
+    println!("Provider connected to {}", provider.url().as_str());
+
+    Ok(())
 }
